6d679ca5aec42d75ba3cc3c5e9172cde3aa5c48e
Marymezh <maria.mezhova@yahoo.com>
2021-08-30 15:01:39 +0700

    added opacity to the button pressed with delay

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
     @IBAction func keyPressed(_ sender: UIButton) {
         print(sender.currentTitle as Any)
         
+        sender.alpha = 0.5
+
+          DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+              sender.alpha = 1.0
+          }
+        
         guard let soundSender = sender.currentTitle else {return}
         playSound(soundName: soundSender)
     }
